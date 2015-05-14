1e2428853324162cb2fbcfa13da83fe38c6cdbb8
lanac <lars@naesbye.dk>
2015-05-14 23:00:51 +0200

    prep script planning 2

---
+++ prep.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+# Mars Simulation Project
+# Script to prepare mars-sim for release
+# transition.py
+# @version 3.08 2015-05-15
+# @author Lars Næsbye Christensen
+#
+# A very simple script that runs the other tool scripts in succession
+#
+
+import io
+import os
+import sys
+
+
+# do sanity check
+# run i18n to prepare localized strings
+# run landmarks and later move the file in place
+# run htmlgenerate to make guide files
+
