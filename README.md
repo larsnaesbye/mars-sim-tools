1089c2fc5558b15cdd221cdf7caf7ec7e5c82b0f
lanac <lars@naesbye.dk>
2015-05-14 23:30:21 +0200

    minor fix

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mars-sim-tools
-A few Python tools for the Mars Simulation Project, including a converter from USGS CSV to mars-sim XML
+A few Python tools for the Mars Simulation Project, including a converter from USGS TSV to mars-sim XML
--- htmlgenerate/htmlgenerate.py
+++ htmlgenerate/htmlgenerate.py
@@ -1,18 +1,18 @@
 # coding=utf-8
 # Mars Simulation Project
-# Script for making HTML5 user guide files for resources and processes 
+# Script for making the HTML5 user guide files for resources and processes
+# replaces the previous java-based internal code
 # htmlgenerate.py
-# @version 3.08 2015-05-13
+# @version 3.08 2015-05-14
 # @author Lars Næsbye Christensen
 #
-#
 # Usage : 'python htmlgenerate.py' 
-
-# no actual code yet, still in design phase
 # must match the current '-help' functionality currently in the Java code
 
 import io
 import os
 import sys
 
+xmldir = sys.argv[1]; // path to XML files from command line 
+htmldir = sys.argv[2]; // path to generated HTML files from command line
 
