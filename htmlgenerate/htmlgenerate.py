a730d273f3a5c1bf954615eb68d606c6c4a3a088
Lars Næsbye Christensen <lars@naesbye.dk>
2015-05-26 19:57:07 +0200

    initial opening of local file, header generation

--- htmlgenerate/htmlgenerate.py
+++ htmlgenerate/htmlgenerate.py
@@ -3,16 +3,32 @@
 # Script for making the HTML5 user guide files for resources and processes
 # replaces the previous java-based internal code
 # htmlgenerate.py
-# @version 3.08 2015-05-14
+# @version 3.08 2015-05-26
 # @author Lars Næsbye Christensen
 #
-# Usage : 'python htmlgenerate.py' 
+# Usage : 'python htmlgenerate.py [xmldir] [htmldir]' 
 # must match the current '-help' functionality currently in the Java code
 
 import io
 import os
 import sys
 
-xmldir = sys.argv[1]; // path to XML files from command line 
-htmldir = sys.argv[2]; // path to generated HTML files from command line
+# TODO: we should check for empty args first
+xmldir = sys.argv[1]; # path to XML files from command line 
+htmldir = sys.argv[2]; # path to generated HTML files from command line
 
+# Open our sample file
+f = open('resources.xml')
+g = open('resources.html','w')
+
+lines = f.readlines() # and read the lines
+
+docheader ="<!DOCTYPE html>\n<head><title>Generic title</title>\n</head>"
+
+for line in lines:
+	if line == "\n":
+		print "Skipped empty line..."  # ignore empty lines
+	else:
+		print line
+f.close()
+g.close()
